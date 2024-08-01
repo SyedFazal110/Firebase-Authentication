5fe67be30490f8ca28ba9798881bc445bbf0df42
SyedFazal110 <syedfazal808@gmail.com>
2024-08-02 00:08:14 +0500

    js file created

--- home.js
+++ home.js
@@ -1,6 +1,14 @@
 import { onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth, db } from "./firebaseconfig.js"
-import { collection, addDoc , getDocs } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { 
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  Timestamp
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -16,7 +24,7 @@ onAuthStateChanged(auth, (user) => {
 const logout = document.querySelector("#logout-btn");
 const todo = document.querySelector("#todo");
 const addBtn = document.querySelector("#add-btn");
-const added = document.querySelector("#list");
+const added = document.querySelector("#data");
 
 
 const arr = [];
@@ -33,31 +41,80 @@ logout.addEventListener('click' , ()=>{
 
 addBtn.addEventListener("click",async (event)=>{
   event.preventDefault()
-  const docRef = await addDoc(collection(db, "todocollection"), {
-    text: todo.value,
-  });
-  console.log("Document written with ID: ", docRef.id);
-  arr.push({
-    text: todo.value,
-  })
+  try {
+    const docRef = await addDoc(collection(db, "todocollection"), {
+      text: todo.value,
+      time: Timestamp.fromDate(new Date()),
+    });
+    console.log("Document written with ID: ", docRef.id);
+    arr.push({
+      text: todo.value,
+      id: docRef.id,
+    });
+    render();
+    todo.value = "";
+  } catch (error) {
+    console.error("Document: ", error);
+  }
   render()
-  todo.value = ""
 })
 
-async function render(){
-  added.innerHTML= ""
-  arr.length = 0
+
+async function getData() {
   const querySnapshot = await getDocs(collection(db, "todocollection"));
   querySnapshot.forEach((doc) => {
-    arr.push(doc.data())
-  console.log(doc.id, " => ", doc.data());
-});
+    arr.push({
+      ...doc.data(),
+      id: doc.id
+    });
+  });
+  console.log(arr);
+  render();
+}
 
+getData();
+
+
+async function render(){
+  added.innerHTML= "";
+
+  if (arr.length === 0) {
+    added.innerHTML = `No Data Found`;
+    return;
+  }
+  
   arr.map((item)=>{
     added.innerHTML += `
     <li>${item.text}</li>
+    <button class="edit-btn">Edit</button>
+    <button class="delete-btn">Delete</button>
     `
-    console.log(item.text);
+  })
+  
+  
+  const editBtn = document.querySelectorAll(".edit-btn");
+  const deleteBtn = document.querySelectorAll(".delete-btn");
+
+
+  editBtn.forEach((btn , index)=>{
+    btn.addEventListener("click", async()=>{
+      const newValue = prompt("Enter new Value")
+      const newTodo = doc(db, "todocollection", arr[index].id);
+      await updateDoc(newTodo, {
+        text : newValue 
+      });
+      console.log("Updated");
+      arr[index].text = newValue;
+      render()
+    })
+  })
+
+  deleteBtn.forEach((btn , index) =>{
+    btn.addEventListener("click", async()=>{
+      await deleteDoc(doc(db, "todocollection", arr[index].id));
+      arr.splice(index , 1)
+      render()
+    })
   })
   
 }
